9fd786ed40128e0a18624401c073d30efbb3c197
Vijay8161 <vijaykrishnaneeli@gmail.com>
2024-07-10 02:26:55 +0530

    Update Online.jsx

--- src/components/Online.jsx
+++ src/components/Online.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from "../styles/rightbar.module.css";
-import { } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 
 export default function Online({ user }) {
@@ -9,16 +8,13 @@ export default function Online({ user }) {
         <li className={styles.rightbarFriend}>
             <div className={styles.rightbarProfileImgContainer}>
                 <img className={styles.rightbarProfileImage} src={PF + user.profilePicture} alt="" />
-                <span className={styles.rightbarOnline}>
-                </span>
-
+                <span className={styles.rightbarOnline}></span>
             </div>
             <span className={styles.rightbarUsername}>
                 <Link to={`/profile/${user.username}`}>
                     {user.username}
                 </Link>
-
             </span>
-        </li >
-    )
+        </li>
+    );
 }
