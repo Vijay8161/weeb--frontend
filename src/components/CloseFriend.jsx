88767cb28741df7ed42b7eb85b58cda6463ce21e
Vijay8161 <vijaykrishnaneeli@gmail.com>
2024-07-10 02:27:45 +0530

    Update CloseFriend.jsx

--- src/components/CloseFriend.jsx
+++ src/components/CloseFriend.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from "../styles/closefriend.module.css";
-import { } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 
 export default function Closefriend({ user }) {
